7b21f0e20aa5796d217e45fcfa89f55b90064bc5
Abdomes <91058164+Abdomes@users.noreply.github.com>
2024-02-09 21:45:20 +0500

    add project

--- src/constants/api.js
+++
@@ -1,7 +0,0 @@
-const ROOT = 'https://dummyjson.com/';
-const USERS = 'users';
-const SEARCH = 'filter?key=';
-/*Базовый ul, по которому находится информация о всех пользователях*/
-export const API_USERS = ROOT + USERS;
-/*url по поиску пользователей, к которому добавляются параметры, установленные пользователем*/
-export const API_SEARCH = API_USERS + '/' + SEARCH;
